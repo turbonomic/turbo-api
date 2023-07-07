61f687f80967da0b3559ea81f0f80600456ec91d
andreiastra <andrei.astra@ibm.com>
2023-07-07 12:27:27 +0100

    [TRB-23038]: Add logging to findTarget

--- pkg/client/api_client.go
+++ pkg/client/api_client.go
@@ -229,8 +229,15 @@ func (c *APIClient) login() (*Result, error) {
 	return &response, nil
 }
 
+func (c *APIClient) printTarget(description string, target *api.Target) {
+	glog.V(4).Infof("%s %+v", description, target)
+	for _, inputField := range target.InputFields {
+		glog.V(4).Infof("Input field: %+v", inputField)
+	}
+}
+
 func (c *APIClient) findTarget(target *api.Target) (*api.Target, error) {
-	glog.V(4).Infof("Find target %+v", target)
+	c.printTarget("Find target", target)
 
 	// Get a list of targets from the Turbo server
 	request := c.Get().Resource(api.Resource_Type_Targets).
@@ -264,14 +271,14 @@ func (c *APIClient) findTarget(target *api.Target) (*api.Target, error) {
 	// by comparing the category, target type and identifier fields
 	// target type is regarded the same if the old one only differs by an extra suffix
 	for _, tgt := range targetList {
-		glog.V(4).Infof("Trying to match with target %+v", tgt)
+		c.printTarget("Trying to match with target", &tgt)
 		// array of InputFields
 		for _, inputField := range tgt.InputFields {
 			if inputField.Name == "targetIdentifier" &&
 				inputField.Value == targetId &&
 				tgt.Category == target.Category &&
 				strings.HasPrefix(tgt.Type, target.Type) {
-				glog.V(4).Infof("Found target match", tgt)
+				glog.V(4).Infof("Found target match")
 				return &tgt, nil
 			}
 		}
